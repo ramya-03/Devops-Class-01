6a80a9901bae1768e527db1607f935a7dc72e1e5
ramya-03 <62537770+ramya-03@users.noreply.github.com>
2023-05-10 12:04:17 +0530

    Update second.sh

--- scripts/second.sh
+++ scripts/second.sh
@@ -1,6 +1,10 @@
 #My second script program
 #Read a run time variable and execute the file
 
+#echo "Please enter your name :"
+#read name
+#echo " Hi $name welcome . Have a nice day !"
+
 echo "Please enter your name :"
-read name
-echo " Hi $name welcome . Have a nice day !"
+#read name
+echo " Hi $1 welcome . Have a nice day !"
